8eeec0b895b920ee538327957edaeb24ff5f999e
Daniel Fritsi <ffddani@gmail.com>
2022-09-10 20:01:36 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+__name__ == "__main__" and setup()
---
+++ src/fritsi/py/utils/__init__.py
@@ -0,0 +1,12 @@
+"""
+A library containing various Python utilities.
+"""
+
+__version__ = "0.1"
+
+__author__ = "Daniel Fritsi"
+__author_email__ = "ffddani@gmail.com"
+
+__license__ = "BSD-3-Clause"
+
+__url__ = "https://github.com/fritsi/py-utils"
